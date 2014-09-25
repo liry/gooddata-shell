c7d10387faeba3f9e47b4a64b9d6685155f9f6d1
Martin Caslavsky <mcaslavsky@gmail.com>
2014-09-25 21:31:57 +0200

    mask password

--- src/main/java/cz/geek/gooddata/shell/commands/LoginCommand.java
+++ src/main/java/cz/geek/gooddata/shell/commands/LoginCommand.java
@@ -3,10 +3,14 @@ package cz.geek.gooddata.shell.commands;
 import com.gooddata.account.Account;
 import com.gooddata.project.Project;
 import cz.geek.gooddata.shell.components.GoodDataHolder;
+import jline.console.ConsoleReader;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  */
@@ -21,10 +25,14 @@ public class LoginCommand extends GoodDataCommand {
     @CliCommand(value = "login", help = "Login to GoodData platform")
     public String login(
             @CliOption(key = {"user"}, mandatory = true, help = "User name") String user,
-            @CliOption(key = {"pass"}, mandatory = true, help = "Password") String pass,
+            @CliOption(key = {"pass"}, mandatory = false, help = "Password") String pass,
             @CliOption(key = {"host"}, mandatory = false, help = "Host") String host,
-            @CliOption(key = {"project"}, mandatory = false, help = "Current project") String projectId) {
+            @CliOption(key = {"project"}, mandatory = false, help = "Current project") String projectId) throws IOException {
 
+        if (StringUtils.isBlank(pass)) {
+            final ConsoleReader cr = new ConsoleReader();
+            pass = cr.readLine("Password: ", '*');
+        }
         holder.login(host, user, pass);
         final Account current = holder.getGoodData().getAccountService().getCurrent();
         if (projectId != null) {
