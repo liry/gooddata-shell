31163b19579bd2caf91bbc19d7afbf16e7611c73
Martin Caslavsky <mcaslavsky@gmail.com>
2014-10-05 00:51:39 +0200

    use output for report export

--- src/main/java/cz/geek/gooddata/shell/commands/ReportCommand.java
+++ src/main/java/cz/geek/gooddata/shell/commands/ReportCommand.java
@@ -1,18 +1,25 @@
 package cz.geek.gooddata.shell.commands;
 
-import com.gooddata.md.MetadataService;
 import com.gooddata.md.report.ReportDefinition;
 import com.gooddata.report.ReportExportFormat;
-import com.gooddata.report.ReportService;
 import cz.geek.gooddata.shell.components.GoodDataHolder;
+import cz.geek.gooddata.shell.output.RowExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+import org.supercsv.io.CsvListReader;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.supercsv.prefs.CsvPreference.STANDARD_PREFERENCE;
 
 /**
  */
@@ -31,15 +38,29 @@ public class ReportCommand extends AbstractGoodDataCommand {
 
 
     @CliCommand(value = "report execute", help = "Execute report")
-    public String report(@CliOption(key = {"uri", ""}, mandatory = true, help = "uri") String uri) {
+    public String report(@CliOption(key = {"uri", ""}, mandatory = true, help = "uri") String uri) throws IOException {
 
-        final MetadataService md = getGoodData().getMetadataService();
-        final ReportDefinition rd = md.getObjByUri(uri, ReportDefinition.class);
+        final ReportDefinition rd = getGoodData().getMetadataService().getObjByUri(uri, ReportDefinition.class);
 
-        final ReportService service = getGoodData().getReportService();
         final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        service.exportReport(rd, ReportExportFormat.CSV, output);
-        return new String(output.toByteArray(), StandardCharsets.UTF_8);
+        getGoodData().getReportService().exportReport(rd, ReportExportFormat.CSV, output);
+
+        final ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
+        final List<List<String>> result = new ArrayList<>();
+        List<String> header = null;
+        try (CsvListReader reader = new CsvListReader(new InputStreamReader(input, UTF_8), STANDARD_PREFERENCE)) {
+            header = reader.read();
+            List<String> row;
+            while ((row = reader.read()) != null) {
+                result.add(row);
+            }
+        }
+        return print(result, header, new RowExtractor<List<String>>() {
+            @Override
+            public List<?> extract(List<String> row) {
+                return row;
+            }
+        });
     }
 
     @CliCommand(value = "report list", help = "List reports")
