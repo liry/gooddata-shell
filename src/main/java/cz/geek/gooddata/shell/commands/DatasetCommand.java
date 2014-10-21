fadbb4720d9c1204c0e91ea228e47673b23db395
Martin Caslavsky <martin.caslavsky@gooddata.com>
2014-10-21 17:05:30 +0200

    wip

--- src/main/java/cz/geek/gooddata/shell/commands/DatasetCommand.java
+++ src/main/java/cz/geek/gooddata/shell/commands/DatasetCommand.java
@@ -10,7 +10,6 @@ import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.supercsv.io.CsvListReader;
 
 import java.io.File;
@@ -20,9 +19,11 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 import static com.gooddata.dataset.DatasetManifest.Part;
 import static java.util.Arrays.asList;
+import static org.springframework.util.StringUtils.delimitedListToStringArray;
 import static org.supercsv.prefs.CsvPreference.STANDARD_PREFERENCE;
 
 /**
@@ -46,7 +47,8 @@ public class DatasetCommand extends AbstractGoodDataCommand {
             @CliOption(key = {"dataset", ""}, mandatory = true, help = "Dataset id") String dataset,
             @CliOption(key = {"csv"}, mandatory = true, help = "CSV file") File csv,
             @CliOption(key = {"mode"}, mandatory = false, help = "Upload mode") UploadMode mode,
-            @CliOption(key = {"populates"}, mandatory = false, help = "TODO") String populates
+            @CliOption(key = {"populates"}, mandatory = false, help = "TODO") String populates,
+            @CliOption(key = {"map"}, mandatory = false, help = "TODO") String map
     ) throws IOException {
 
         if (mode == null) {
@@ -59,10 +61,19 @@ public class DatasetCommand extends AbstractGoodDataCommand {
         try (final CsvListReader reader = new CsvListReader(new InputStreamReader(new FileInputStream(csv)), STANDARD_PREFERENCE)) {
             header = reader.getHeader(true);
         }
+        map(manifest, mode, header, populates, map);
+        try (final FileInputStream stream = new FileInputStream(csv)) {
+            service.loadDataset(getCurrentProject(), manifest, stream).get();
+        }
+        return "Loaded dataset: " + dataset;
+    }
+
+    public static void map(final DatasetManifest manifest, final UploadMode mode, final String[] csvHeader,
+                           final String populates, final String map) {
         if (populates != null) {
             final String[] pop = populates.split(",");
-            if (pop.length != header.length) {
-                throw new IllegalArgumentException("CSV header items " + header.length + " != populates items " + pop.length);
+            if (pop.length != csvHeader.length) {
+                throw new IllegalArgumentException("CSV header items " + csvHeader.length + " != populates items " + pop.length);
             }
             final HashMap<String, Part> parts = new HashMap<>(manifest.getParts().size());
             for (Part part: manifest.getParts()) {
@@ -70,39 +81,56 @@ public class DatasetCommand extends AbstractGoodDataCommand {
                     parts.put(p, part);
                 }
             }
-            if (header.length != parts.size()) {
-                throw new IllegalArgumentException("CSV header items " + header.length + " != dataset populates " + parts.size());
+            if (csvHeader.length != parts.size()) {
+                throw new IllegalArgumentException("CSV header items " + csvHeader.length + " != dataset populates " + parts.size());
             }
             // todo this should be part of Manifest class
-            for (int i = 0; i<header.length; i++) {
+            for (int i = 0; i< csvHeader.length; i++) {
                 final Part part = parts.get(pop[i]);
                 if (part == null) {
                     throw new IllegalArgumentException("Unknown part for populate " + pop[i]);
                 }
-                part.setColumnName(header[i]);
+                part.setColumnName(csvHeader[i]);
                 part.setUploadMode(mode.name());
             }
         } else {
-            final HashSet<String> columns = new HashSet<>(asList(header));
+            final Map<String, String> extraMap = new HashMap<>();
+            if (map != null) {
+                for (String pair: delimitedListToStringArray(map, ",")) {
+                    final String[] strings = delimitedListToStringArray(pair, "=");
+                    if (strings.length != 2) {
+                        continue;
+                    }
+                    extraMap.put(strings[0], strings[1]);
+                }
+            }
+            final HashSet<String> columns = new HashSet<>(asList(csvHeader));
             for (Part part: manifest.getParts()) {
                 if (part.getPopulates() == null || part.getPopulates().size() != 1) {
                     throw new IllegalStateException("Only parts with exactly one populates are supported " + part.getPopulates());
                 }
                 final String field = part.getPopulates().iterator().next();
-                final String[] items = StringUtils.delimitedListToStringArray(field, ".");
-                final String column = items == null ? field : items[items.length - 1];
-                if (!columns.contains(column)) {
-                    throw new IllegalStateException("Unable to automap field " + field +
-                            ". CSV file contains columns " + columns + " but not the given column " + column);
+                final String column;
+                if (columns.contains(field)) {
+                    column = field;
+                } else if (extraMap.containsKey(field)) {
+                    column = extraMap.get(field);
+                } else {
+                    final String[] items = delimitedListToStringArray(field, ".");
+                    final String last = items[items.length - 1];
+                    if (columns.contains(last)) {
+                        column = last;
+                    } else if (extraMap.containsKey(last)) {
+                        column = extraMap.get(last);
+                    } else {
+                        throw new IllegalStateException("Unable to map field '" + field + "' ('" + last + "'). " +
+                                "CSV columns: " + columns + " Extra mapping: " + extraMap);
+                    }
                 }
                 part.setColumnName(column);
                 part.setUploadMode(mode.name());
             }
         }
-        try (final FileInputStream stream = new FileInputStream(csv)) {
-            service.loadDataset(getCurrentProject(), manifest, stream).get();
-        }
-        return "Loaded dataset: " + dataset;
     }
 
     @CliCommand(value = "dataset list", help = "List datasets")
