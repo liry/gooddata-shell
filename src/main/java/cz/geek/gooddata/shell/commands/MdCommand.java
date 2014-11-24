6b40b5833a02c3987f96cfbf926cd25b5a941caa
Martin Caslavsky <martin.caslavsky@gooddata.com>
2014-11-24 13:25:01 +0100

    add: find md by title or summary, make type optional

--- src/main/java/cz/geek/gooddata/shell/commands/MdCommand.java
+++ src/main/java/cz/geek/gooddata/shell/commands/MdCommand.java
@@ -1,6 +1,7 @@
 package cz.geek.gooddata.shell.commands;
 
 import com.gooddata.md.Attribute;
+import com.gooddata.md.Entry;
 import com.gooddata.md.Fact;
 import com.gooddata.md.Metric;
 import com.gooddata.md.Queryable;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -46,14 +48,28 @@ public class MdCommand extends AbstractGoodDataCommand {
 
 
     @CliCommand(value = "md", help = "Query metadata")
-    public String md(@CliOption(key = {"find"}, mandatory = true, help = "type") MdType type,
-                     @CliOption(key = {"id"}, mandatory = false, help = "identifier") String id) {
+    public String md(@CliOption(key = {"type"}, mandatory = false, help = "type") MdType type,
+                     @CliOption(key = {"id"}, mandatory = false, help = "identifier") String id,
+                     @CliOption(key = {"title"}, mandatory = false, help = "title") String title,
+                     @CliOption(key = {"summary"}, mandatory = false, help = "summary") String summary
+                     ) {
         final List<Restriction> restrictions = new ArrayList<>();
         if (StringUtils.hasText(id)) {
             restrictions.add(Restriction.identifier(id));
         }
+        if (StringUtils.hasText(title)) {
+            restrictions.add(Restriction.title(title));
+        }
+        if (StringUtils.hasText(summary)) {
+            restrictions.add(Restriction.summary(summary));
+        }
         final Restriction[] restr = restrictions.toArray(new Restriction[restrictions.size()]);
-        return printEntries(getGoodData().getMetadataService().find(getCurrentProject(), type.cls, restr));
+        final List<Entry> entries = new ArrayList<>();
+        for (MdType t: type != null ? new MdType[]{type} : MdType.values()) {
+            final Collection<Entry> result = getGoodData().getMetadataService().find(getCurrentProject(), t.cls, restr);
+            entries.addAll(result);
+        }
+        return printEntries(entries);
     }
 
 }
