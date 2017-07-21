2f3a23f5157b4ddb41f82ea5f2f0aa89f0fc10d3
Martin Caslavsky <mcaslavsky@gmail.com>
2017-07-21 14:19:45 +0200

    add: update account

--- src/main/java/cz/geek/gooddata/shell/commands/AccountCommand.java
+++ src/main/java/cz/geek/gooddata/shell/commands/AccountCommand.java
@@ -3,13 +3,17 @@ package cz.geek.gooddata.shell.commands;
 import com.gooddata.account.Account;
 import com.gooddata.account.AccountService;
 import cz.geek.gooddata.shell.components.GoodDataHolder;
+import cz.geek.gooddata.shell.components.Utils;
 import cz.geek.gooddata.shell.output.RowExtractor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -23,7 +27,7 @@ public class AccountCommand extends AbstractGoodDataCommand {
         super(holder);
     }
 
-    @CliAvailabilityIndicator({"account create", "account get", "account delete"})
+    @CliAvailabilityIndicator({"account create", "account get", "account delete", "account update"})
     public boolean isAvailable() {
         return holder.hasGoodData();
     }
@@ -36,20 +40,42 @@ public class AccountCommand extends AbstractGoodDataCommand {
             @CliOption(key = {"last"}, mandatory = true, help = "Last name") String last,
             @CliOption(key = {"domain"}, mandatory = true, help = "Domain/Organization name") String domain
     ) {
-        final Account account = getGoodData().getAccountService().createAccount(new Account(email, pass, first, last), domain);
+        final Account account = getAccountService().createAccount(new Account(email, pass, first, last), domain);
         return "Created account: " + account.getUri();
     }
 
+    @CliCommand(value = "account update", help = "Update account")
+    public String update(
+            @CliOption(key = {""}, help = "Account id or uri or current account if not given") String account,
+            @CliOption(key = {"first"},help = "First name") String first,
+            @CliOption(key = {"last"}, help = "Last name") String last,
+            @CliOption(key = {"ips"}, help = "IP whitelist (comma separated)") String ip
+    ) throws IOException {
+        final Account a = account != null ? getAccount(account) : getAccountService().getCurrent();
+        a.setEmail(null); // todo this is a bit weird, but doesn't work otherwise
+        if (first != null) {
+            a.setFirstName(first);
+        }
+        if (last != null) {
+            a.setLastName(last);
+        }
+        if (ip != null) {
+            a.setIpWhitelist(Arrays.asList(StringUtils.split(ip, ",")));
+        }
+        getAccountService().updateAccount(a);
+        return "Updated account: " + a.getUri();
+    }
+
     @CliCommand(value = "account get", help = "Get account")
-    public String project(@CliOption(key = {""}, help = "Account id or uri") String account) {
-        final Account a = account != null ? getAccount(account) : getGoodData().getAccountService().getCurrent();
+    public String project(@CliOption(key = {""}, help = "Account id or uri or current account if not given") String account) {
+        final Account a = account != null ? getAccount(account) : getAccountService().getCurrent();
         return print(singletonList(a));
     }
 
     @CliCommand(value = "account delete", help = "Delete account")
     public String delete(@CliOption(key = {""}, mandatory = true, help = "Account id or uri") String account) {
         final Account a = getAccount(account);
-        getGoodData().getAccountService().removeAccount(a);
+        getAccountService().removeAccount(a);
         return "Removed " + a.getUri();
     }
 
@@ -60,6 +86,10 @@ public class AccountCommand extends AbstractGoodDataCommand {
                 return asList(account.getUri(), account.getLogin(), account.getFirstName(), account.getLastName());
             }
         });
+    }
+
+    private AccountService getAccountService() {
+        return getGoodData().getAccountService();
     }
 
 }
