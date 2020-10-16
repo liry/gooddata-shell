8c72e44b4696ed40cd1c07f95b7e54232e3615fc
Libor Rysavy <liry@users.noreply.github.com>
2020-10-16 13:23:27 +0200

    Fix creation of directory on Windows OS

--- src/main/java/cz/geek/gooddata/shell/components/GoodDataHistoryFileNameProvider.java
+++ src/main/java/cz/geek/gooddata/shell/components/GoodDataHistoryFileNameProvider.java
@@ -9,32 +9,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.HashSet;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.EnumSet;
 import java.util.Set;
 
 import static java.nio.file.attribute.PosixFilePermission.*;
-import static java.nio.file.attribute.PosixFilePermissions.asFileAttribute;
-import static java.util.Arrays.asList;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class GoodDataHistoryFileNameProvider implements HistoryFileNameProvider {
 
-    private static final FileAttribute<Set<PosixFilePermission>> ATTRS = asFileAttribute(new HashSet<>(asList(
-                    OWNER_READ, OWNER_WRITE, OWNER_EXECUTE)));
+    private static final Set<PosixFilePermission> PERMS = EnumSet.of(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE);
 
     public String getHistoryFileName() {
         final Path dir = Paths.get(System.getProperty("user.home"), ".gooddatashell");
-        try {
-            Files.createDirectories(dir, ATTRS);
-        } catch (IOException ignored) {
+        if (!dir.toFile().exists()) {
+            try {
+                if (isWindows()) {
+                    Files.createDirectories(dir);
+                } else {
+                    Files.createDirectories(dir, PosixFilePermissions.asFileAttribute(PERMS));
+                }
+            } catch (IOException ignored) {
+            }
         }
         return dir.resolve("history").toString();
-	}
+    }
 
-	public String getProviderName() {
-		return "gooddata-history-provider";
-	}
+    public String getProviderName() {
+        return "gooddata-history-provider";
+    }
+
+    private static boolean isWindows() {
+        return System.getProperty("os.name").contains("Windows");
+    }
 }
